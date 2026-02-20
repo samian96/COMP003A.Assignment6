a7c76538dd1497aa4507fc522fdc256a3b7e10c1
samian <sam.iannazzo@yahoo.com>
2026-02-19 21:29:20 -0800

    added class BookTracker / test string and int named

---
+++ COMP003A.Assignment6/BookTracker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Assignment6
+{
+    internal class BookTracker
+    {
+        string Name;
+
+        int File;
+    }
+}
