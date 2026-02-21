96733fcb2c5e28a8552b47f8ca36d0c1a3062d84
samian <sam.iannazzo@yahoo.com>
2026-02-20 20:21:00 -0800

    added comment to booktracker

--- COMP003A.Assignment6/BookTracker.cs
+++ COMP003A.Assignment6/BookTracker.cs
@@ -5,6 +5,10 @@ using System.Text;
 namespace COMP003A.Assignment6
 {
     internal class BookTracker
+        /* The class BookTracker is here to hold the different data types for the books, how many pages, as well as the availability
+         * this will be called to from the main class, this helps clean up the main code from becoming too cluttered.
+         * Though my main code is still somewhat cluttered this class still cuts down on the clutter.
+         */
     {
         public string Book;
 
